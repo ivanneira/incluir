839e63a7d9f1ad16df981fa1e479964d36f011a6
DESKTOP-BANPLN5\Sebas <pseba20@gmail.com>
2018-06-22 09:21:05 -0300

    Botones Editar y Detalle de planila y encuesta.
    Se agregaron fa icons

--- public/javascripts/urls.js
+++ public/javascripts/urls.js
@@ -2,12 +2,13 @@
  * Created by Sebas on 18/6/2018.
  */
 
-
+/*
 var server_url = "/aresapi";
 var server_host = "http://10.64.65.200";
 var server_port = 80;
-/*
+*/
+
+
 var server_url = "";
 var server_host = "http://localhost";
 var server_port = 1941;
-    */
